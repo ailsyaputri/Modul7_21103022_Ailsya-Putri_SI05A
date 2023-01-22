17429614b576acf8a16e0feacec8ef9c391c4e57
Ailsya Putri <ailsyaputri9597@gmail.com>
2023-01-22 21:28:10 +0700

    Komentar

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,14 @@
+<nav>
+    <header>
+        <div class="d-flex justify-content-between">
+            <h1>
+                TOKO
+            </h1>
+            <ul>
+                <li>Menu 1</li>
+                <li>Menu 2</li>
+                <li>Menu 3</li>
+            </ul>
+        </div> 
+    </header>
+</nav>
